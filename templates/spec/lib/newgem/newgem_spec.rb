fb6bb50c09cc65a2cc0a0047ab0075061ebbb8be
Nick den Engelsman <nickdenengelsman@gmail.com>
2014-08-14 16:56:29 +0200

    Add command line option to specify different test framework

---
+++ templates/spec/lib/newgem/newgem_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../../spec_helper'
+
+describe <%= MiscUtils.camel_case(name) %> do
+  context '#dummy_test' do
+    it 'passes the simple test' do
+      expect('test string').to_not be_nil
+    end
+  end
+end
---
+++ templates/spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require 'pry'
+require 'awesome_print'
+require_relative '../lib/<%= name %>'
+include <%= MiscUtils.camel_case(name) %>
+
+RSpec.configure do |config|
+  config.color = true
+  config.formatter = 'documentation'
+end
