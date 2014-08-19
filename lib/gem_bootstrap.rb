c222752760c150698da92c562920b1fa27e051bf
Nick den Engelsman <nickdenengelsman@gmail.com>
2014-08-19 14:36:31 +0200

    Use default Rubocop style guide and improve Guardfile for rspec

--- lib/gem_bootstrap.rb
+++ lib/gem_bootstrap.rb
@@ -1,6 +1,6 @@
-require_relative "./gem_bootstrap/version"
-require_relative "./gem_bootstrap/cli"
-require_relative "./gem_bootstrap/git_utils"
-require_relative "./gem_bootstrap/misc_utils"
-require_relative "./gem_bootstrap/gem_bootstrap"
+require_relative './gem_bootstrap/version'
+require_relative './gem_bootstrap/cli'
+require_relative './gem_bootstrap/git_utils'
+require_relative './gem_bootstrap/misc_utils'
+require_relative './gem_bootstrap/gem_bootstrap'
 include GemBootstrap
